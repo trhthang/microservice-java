eb5b7e1e17245636c06a5ebe32907589536cfcc2
trhthang <trhthang0401@gmail.com>
2024-05-08 17:05:17 +0900

    update

---
+++ cards/src/main/java/com/eazybytes/cards/entity/BaseEntity.java
@@ -0,0 +1,11 @@
+package com.eazybytes.cards.entity;
+
+import java.time.LocalDateTime;
+
+public class BaseEntity {
+
+    private LocalDateTime createAt;
+    private String createBy;
+    private LocalDateTime updateAt;
+    private String updateBy;
+}
