a09f183a7ffe8cebe990d4dc60f72b212cc3c7b8
trhthang <trhthang0401@gmail.com>
2024-06-22 16:35:13 +0900

    update

---
+++ making microservices resilient/cards/src/main/java/com/eazybytes/cards/entity/Cards.java
@@ -0,0 +1,39 @@
+package com.eazybytes.cards.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.GenericGenerator;
+
+@Entity
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+public class Cards extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO,generator = "native")
+    @GenericGenerator(name = "native", strategy = "native")
+    @Column(name = "card_id")
+    private Long cardId;
+
+    @Column(name = "mobile_number")
+    private String mobileNumber;
+
+    @Column(name = "card_number")
+    private String cardNumber;
+
+    @Column(name = "card_type")
+    private String cardType;
+
+    @Column(name = "total_limit")
+    private int totalLimit;
+
+    @Column(name = "amount_used")
+    private int amountUsed;
+
+    @Column(name = "available_amount")
+    private int availableAmount;
+
+}
---
+++ making microservices resilient/loans/src/main/java/com/eazybytes/loans/LoansApplication.java
@@ -0,0 +1,35 @@
+package com.eazybytes.loans;
+import com.eazybytes.loans.dto.LoansContactInfoDto;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@SpringBootApplication
+/*@ComponentScans({ @ComponentScan("com.eazybytes.loans.controller") })
+@EnableJpaRepositories("com.eazybytes.loans.repository")
+@EntityScan("com.eazybytes.loans.model")*/
+@EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
+@EnableConfigurationProperties(value = {LoansContactInfoDto.class})
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Loans microservice REST API Documentation",
+				description = "EazyBank Loans microservice REST API Documentation",
+				version = "v1",
+				contact = @Contact(
+						name = "Huy Thang Tran",
+						email = "trhthang0401@gmail.com",
+						url = "https://www.eazybytes.com"
+				)
+		)
+)
+public class LoansApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoansApplication.class, args);
+	}
+
+}
